ed5831b503dc12bea61dad8ef664abf1da0732e0
DanTheMan827 <790119+DanTheMan827@users.noreply.github.com>
2018-01-11 10:18:22 -0600

    Clean up code and cache the file list

--- Random_Game.sh
+++ Random_Game.sh
@@ -6,42 +6,54 @@
 source /etc/preinit
 script_init
 
+cachePath="/tmp/RandomGame-FileList"
+lastLaunchArgsPath="/tmp/RandomGame-LaunchArguments"
+lastCommandPath="/tmp/RandomGame-LastCommand"
+
 script_gamecode="$(basename $(dirname $0))"
 
-if [ "$(echo "$*" | grep "rollback-input-dir")" != "" ]; then
-  exec $(cat /tmp/RandomGame-LastCommand) $@
+if (echo "$*" | grep -qEe "rollback-input-dir|load-state"); then
+  exec $(cat "$lastCommandPath") $@
 fi
 
 gameStorage="$rootfs$gamepath"
 type findGameStorage && gameStorage="$(findGameStorage)"
 
-flist="$(find "$gameStorage" -name "*.desktop" | grep -ve "$script_gamecode")"
+if [ -f "$cachePath" ]; then
+  fileList="$(cat "$cachePath")"
+else
+  fileList="$(find "$gameStorage" -name "*.desktop")"
+  echo "$fileList" > "$cachePath"
+fi
+
+fileList="$(echo "$fileList" | grep -ve "$script_gamecode")"
+[ "$sftype" != "nes" ] && fileList="$(echo "$fileList" | grep -ve "^$gameStorage/nes/")"
+
 while true
 do
-  dfile="$(echo "$flist" | head -$((${RANDOM} % `echo "$flist" | wc -l` + 1)) | tail -1)"
-  grep "$dfile" -qEe "chmenu|^Random=false|/bin/hsqs |/bin/sh" || break
-  [ "$sftype" != "nes" ] && (grep "$dfile" -qEe "^$gameStorage/nes/" || break)
+  desktopFile="$(echo "$fileList" | head -$((${RANDOM} % `echo "$fileList" | wc -l` + 1)) | tail -1)"
+  grep "$desktopFile" -qEe "chmenu|^Random=false|/bin/hsqs |/bin/sh " || break
 done
 
-eline="$(cat "$dfile" | grep -E '^Exec=')"
-pline="$(cat "$dfile" | grep -E '^Path=')"
-cline="$(cat "$dfile" | grep -E '^Code=')"
-fpath="$(dirname "$dfile")"
-dcommand="${eline##Exec=}"
-dpath="${pline##Path=}"
-dcode="${cline##Code=}"
-dcommand="$(echo "$dcommand" | sed -e "s# \(/usr/share/games/\(nes/kachikachi/\)\?\|/var/games/\)$dcode/# $fpath/#")"
-echo "$dcommand"
+execLine="$(cat "$desktopFile" | grep -E '^Exec=')"
+pathLine="$(cat "$desktopFile" | grep -E '^Path=')"
+codeLine="$(cat "$desktopFile" | grep -E '^Code=')"
+desktopFilePath="$(dirname "$desktopFile")"
+desktopExec="${execLine##Exec=}"
+desktopPath="${pathLine##Path=}"
+desktopCode="${codeLine##Code=}"
+desktopExec="$(echo "$desktopExec" | sed -e "s# \(/usr/share/games/\(nes/kachikachi/\)\?\|/var/games/\)$desktopCode/# $desktopFilePath/#")"
+echo "$desktopExec"
 pwd="$(pwd)"
 
-mkdir -p "$dpath"
+mkdir -p "$desktopPath"
 
 cd /
 umount "$pwd" 2>/dev/null
-mount_bind "$dpath" "$pwd"
+mount_bind "$desktopPath" "$pwd"
 cd "$pwd"
 
-echo "$@" > "/tmp/RandomGame-LaunchArguments"
-echo "$dcommand" > "/tmp/RandomGame-LastCommand"
+echo "$@" > "$lastLaunchArgsPath"
+echo "$desktopExec" > "$lastCommandPath"
 
-exec $dcommand $@
+exec $desktopExec $@
